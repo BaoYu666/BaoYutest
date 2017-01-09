f9e6b38db1f309adf43f97c48ab6be2457740fc0
NiuYunfang <842041605@qq.com>
2017-01-09 19:07:18 +0800

    核心代码1.1
    添加主函数，上次忘了，，，

---
+++ 核心内容/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include "Station.h"
+#include "Graph.h"
+#include "Dijkstra.h"
+#include "Function.h"
+using namespace std;
+
+
+using namespace std;
+
+
+int main()
+{
+    cout<<endl<<"\t\t\t\t欢迎使用路由表生成查询系统！"<<endl;
+    FileIn();//从文件导入
+    while(1)
+    {
+        if(!PrintChoose()) break;
+    }
+}
+
+
